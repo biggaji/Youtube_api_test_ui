7475b6d668bc13a6427d71bcfd422a4b6fc52824
Tobi Ajibade <herityjohnny14>
2021-01-04 00:13:53 -0800

    Youtube functionality created: subscribe data

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const dataUi = document.querySelector(".dropdown_data_ui");
 const body = document.body;
 const darkModeBtn = document.querySelector(".darkmode");
 const greetings = document.querySelector(".greeting_text");
-
+const yt_sub_count = document.querySelector(".yt_sub_count");
 (function () {
     youtubeUsername.addEventListener("click", function () {
         dataUi.classList.toggle("renderUi");
@@ -29,15 +29,27 @@ const hourOfTheDay = new Date().getHours(24);
 
 (() => {
     if (hourOfTheDay >= 12 && hourOfTheDay <= 18) {
-        greetings.innerHTML = "Hi, Good Afternoon!"
-        // } else if (hourOfTheDay < 19) {
-        //     greetings.innerHTML = "Hi, Good Morning!"
+        greetings.innerHTML = "Hi, Good Afternoon!";
     } else if (hourOfTheDay >= 19 && hourOfTheDay <= 24) {
         body.classList.toggle("toggleDarkmode");
         greetings.innerHTML = "Hi, Good Evening";
     } else {
-        // darkModeBtn.style.display = "none";
-        // greetings.innerHTML = "Hi, How are you doing?"
         greetings.innerHTML = "Hi, Good Morning!"
     }
 })()
+
+// making fetch request to the youtube api
+
+const url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&key=AIzaSyDDr11G0rgShLPEXmNpI66raLdsl83LxYM&id=UCaNTxRkQ01gMBnL1mgnM_KA";
+(() => {
+    fetch(url)
+        .then(resp => {
+            return resp.json();
+        })
+        .then(channelData => {
+            yt_sub_count.innerHTML = channelData.items[0].statistics.subscriberCount;
+        })
+        .catch(e => {
+            console.log("An error occured while fetching channel data ", e);
+        });
+})();
